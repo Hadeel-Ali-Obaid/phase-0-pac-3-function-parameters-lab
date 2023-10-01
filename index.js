e761eacd22292ae68f4a521fdefdeb9b1d3b7c2e
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-10-01 14:24:47 +0300

    add index.js and package-lock

---
+++ index.js
@@ -0,0 +1,15 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+function introductionWithLanguageOptional(name, language) {
+    let defaultLanguage = 'JavaScript';
+    if (language) {
+        defaultLanguage = language;
+    } 
+    return `Hi, my name is ${name} and I am learning to program in ${defaultLanguage}.`
+}
