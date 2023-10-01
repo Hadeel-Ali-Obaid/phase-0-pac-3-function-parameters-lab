1a75c7aab0bdde455615a74f45b77f36ae3b2ac2
Hadeel Obaid <hadeelobaid71@gmail.com>
2023-10-01 14:43:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ function introductionWithLanguage(name, language) {
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
 
-function introductionWithLanguageOptional(name, language) {
+
+/*function introductionWithLanguageOptional(name, language) {
     let defaultLanguage = 'JavaScript';
     if (language) {
         defaultLanguage = language;
     } 
     return `Hi, my name is ${name} and I am learning to program in ${defaultLanguage}.`
+}*/
+
+
+function introductionWithLanguageOptional(name, language="JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
